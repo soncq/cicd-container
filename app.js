93aa673412a436553f6b4851f43f3a3db5a72d14
soncq <quocsoncao@gmail.com>
2023-03-22 11:50:55 +0700

    Change name

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Curtis Cao!"));
+app.get("/", (req, res) => res.send("Hello Curtis!"));
 
 app.get("/health", (req, res) => {
   res.status(200);
